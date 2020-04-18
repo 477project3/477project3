f538c79cd10107fbceecdb01d435b9f528b39c90
477project3 <ying+477project3@users.noreply.github.com>
2020-04-18 18:51:07 -0400

    Add startcode
    Add startcode

---
+++ project3/Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject backwall;
+    public GameObject player;
+
+    public float min_speed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 movement = new Vector3(min_speed, 0, 0) * Time.deltaTime;
+        Vector3 movement = new Vector3(0, 0, 0) * Time.deltaTime;
+
+        // Speed up to maintain pace with the player if the player moves forward
+        float catchup = player.transform.position.x - transform.position.x + Camera.main.orthographicSize;
+        if (catchup > 0) { movement.x += (float) System.Math.Pow(catchup, 2) * Time.deltaTime / 5; }
+
+        // Adapt to the Y position of the player
+        float y_diff = transform.position.y - player.transform.position.y;
+        if (System.Math.Abs(y_diff) > Camera.main.orthographicSize / 5) { movement.y -= (float) System.Math.Pow(y_diff, 2) * System.Math.Sign(y_diff) * Time.deltaTime; }
+
+        transform.position += movement;
+        backwall.transform.position = transform.position + new Vector3(-11.2f, 0, 0);
+    }
+}
---
+++ project3/Assets/Scripts/GameLogic.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLogic : MonoBehaviour
+{
+
+    public GameObject ball;
+
+    private List<GameObject> balls;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        balls = new List<GameObject>();
+        for (int i = 0; i < 1; i++)
+        {
+            balls.Add(Instantiate(ball));
+            balls[i].transform.position = spawn_ball_position();
+            balls[i].GetComponent<Rigidbody2D>().velocity = spawn_ball_velocity();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject b;
+        CircleCollider2D cc;
+        for (int i = 0; i < balls.Count; i++)
+        {
+            b = balls[i];
+            cc = b.GetComponent<CircleCollider2D>();
+            if (cc.bounds.center.y + cc.bounds.extents.y < Camera.main.transform.position.y - Camera.main.orthographicSize)
+            {
+                // Re-initialize the object
+                b.transform.position = spawn_ball_position();
+                b.GetComponent<Rigidbody2D>().velocity = spawn_ball_velocity();
+            }
+        }
+    }
+
+    Vector3 spawn_ball_velocity()
+    {
+        return new Vector3(Random.Range(-5, 3), Random.Range(-5, 0), 0);
+    }
+
+    Vector3 spawn_ball_position()
+    {
+        return new Vector3(Camera.main.transform.position.x + Random.Range(0f, Camera.main.orthographicSize * 1), Camera.main.transform.position.y + Camera.main.orthographicSize / 2, 0);
+    }
+}
