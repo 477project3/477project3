be793f4640f3e259016626312f2d6cf29a994b0b
477project3 <63932785+477project3@users.noreply.github.com>
2020-04-24 15:10:29 -0400

    more roadblock added

---
+++ Assets/Scripts/dc.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dc : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            UIManager.Instance.AddHealth(-5);
+        }
+    }
+}
